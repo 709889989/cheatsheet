772c0270d208dbc533cbe68e97afdbf02ad478a2
xu.mingming <xu.mingming@99bill.com>
2018-06-06 15:34:51 +0800

    update 添加文档模板

---
+++ document/应用功能拆分思路.mm
@@ -0,0 +1,308 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1520218613586" ID="ID_1602317323" MODIFIED="1520218667785" TEXT="HAT &#x5e94;&#x7528;">
+<node CREATED="1520220761468" ID="ID_684034241" MODIFIED="1520235101949" POSITION="right" TEXT="&#x7f51;&#x5173;">
+<node CREATED="1520235009286" ID="ID_1083328311" MODIFIED="1520421139450" TEXT="app-hat-gateway API&#x7f51;&#x5173;">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#38472;&#20142;
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="button_ok"/>
+<node CREATED="1520237126960" ID="ID_608019182" MODIFIED="1520237128357" TEXT="&#x5b89;&#x5168;"/>
+<node CREATED="1520237128744" ID="ID_346922377" MODIFIED="1520237131768" TEXT="&#x6807;&#x8bc6;&#x8f6c;&#x6362;"/>
+<node CREATED="1520237133154" ID="ID_664822133" MODIFIED="1520237141026" TEXT="&#x53c2;&#x6570;&#x6620;&#x5c04;"/>
+</node>
+<node CREATED="1520235012483" ID="ID_20795562" MODIFIED="1520421149696" TEXT="app-hat-notify-gateway &#x901a;&#x77e5;&#x7f51;&#x5173;">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#38472;&#20142;
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="button_ok"/>
+<node CREATED="1520237105985" ID="ID_1114991240" MODIFIED="1520237109310" TEXT="&#x5b89;&#x5168;"/>
+<node CREATED="1520237109643" ID="ID_1323384083" MODIFIED="1520237115176" TEXT="&#x6807;&#x8bc6;&#x8f6c;&#x6362;"/>
+<node CREATED="1520237116218" ID="ID_90248407" MODIFIED="1520237123846" TEXT="&#x4fdd;&#x8bc1;&#x901a;&#x77e5;&#x6210;&#x529f;"/>
+</node>
+</node>
+<node CREATED="1520230758651" ID="ID_362877731" MODIFIED="1520230762770" POSITION="right" TEXT="&#x5b9e;&#x4f53;">
+<node CREATED="1520218727324" ID="ID_1401616322" MODIFIED="1520419913905" TEXT="app-hat-partner&#x5408;&#x4f5c;&#x65b9;&#x5e73;&#x53f0;">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#21346;&#28925;
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="button_ok"/>
+<node CREATED="1520234447450" ID="ID_1219293906" MODIFIED="1520234450671" TEXT="&#x5f00;&#x901a;"/>
+<node CREATED="1520234453258" ID="ID_7263628" MODIFIED="1520234459342" TEXT="&#x5173;&#x95ed;"/>
+<node CREATED="1520234450967" ID="ID_1458457891" MODIFIED="1520234452953" TEXT="&#x67e5;&#x8be2;"/>
+</node>
+<node CREATED="1520218692611" ID="ID_113516580" MODIFIED="1520234542722" TEXT="&#x4e2a;&#x4eba;">
+<node CREATED="1520234583555" ID="ID_697951064" MODIFIED="1520420500988" TEXT="app-hat-member-personal&#x4e2a;&#x4eba;">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#24352;&#36745;
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="button_ok"/>
+<node CREATED="1520232360444" ID="ID_164605839" MODIFIED="1520232367528" TEXT="&#x5f00;&#x6237;"/>
+<node CREATED="1520051038922" ID="ID_118146020" MODIFIED="1520051045488" TEXT="&#x5347;&#x7ea7;&#x4e2a;&#x4eba;&#x5356;&#x5bb6;"/>
+<node CREATED="1520051081522" ID="ID_758964529" MODIFIED="1520051083669" TEXT="&#x9500;&#x6237;"/>
+<node CREATED="1520050461464" ID="ID_1406366352" MODIFIED="1520050489244" TEXT="&#x67e5;&#x8be2;&#x4f1a;&#x5458;&#x4fe1;&#x606f;"/>
+</node>
+<node CREATED="1520046930851" ID="ID_895073311" MODIFIED="1520420507996" TEXT="app-hat-member-personal-bankcard&#x7ed1;&#x5361;">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#23828;&#20271;&#23493;
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="button_ok"/>
+<node CREATED="1520046984393" ID="ID_265481888" MODIFIED="1520046992370" TEXT="&#x7ed1;&#x5361;">
+<node CREATED="1520047250636" ID="ID_1202040422" MODIFIED="1520047258569" TEXT="&#x9274;&#x6743;"/>
+<node CREATED="1520047264306" ID="ID_1624670960" MODIFIED="1520047268427" TEXT="&#x7b7e;&#x7ea6;"/>
+</node>
+<node CREATED="1520046993729" ID="ID_974260022" MODIFIED="1520046995789" TEXT="&#x89e3;&#x7ed1;"/>
+<node CREATED="1520046996468" ID="ID_462221287" MODIFIED="1520047001738" TEXT="&#x67e5;&#x8be2;&#x7ed1;&#x5361;&#x5217;&#x8868;"/>
+<node CREATED="1520056925169" ID="ID_904899720" MODIFIED="1520057020392" TEXT="&#x67e5;&#x8be2;&#x652f;&#x6301;&#x7684;&#x94f6;&#x884c;&#x5361;&#x5217;&#x8868;"/>
+<node CREATED="1520047002226" ID="ID_779881498" MODIFIED="1520057027842" TEXT="&#x67e5;&#x8be2;&#x94f6;&#x884c;&#x53d7;&#x7406;&#x80fd;&#x529b;"/>
+</node>
+</node>
+<node CREATED="1520218786902" ID="ID_1325436373" MODIFIED="1520234413267" TEXT="&#x4f01;&#x4e1a;">
+<node CREATED="1520234716048" ID="ID_1672898681" MODIFIED="1520420515954" TEXT="app-hat-member-enterprise&#x4f01;&#x4e1a;">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#36213;&#26149;&#26472;
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="button_ok"/>
+<node CREATED="1520233551321" ID="ID_307803754" MODIFIED="1520233561966" TEXT="&#x5f00;&#x6237; "/>
+<node CREATED="1520051087144" ID="ID_1589469807" MODIFIED="1520051088673" TEXT="&#x9500;&#x6237;"/>
+<node CREATED="1520051022234" ID="ID_1218629002" MODIFIED="1520051029166" TEXT="&#x67e5;&#x8be2;&#x4f1a;&#x5458;&#x4fe1;&#x606f;"/>
+</node>
+<node CREATED="1520051091347" ID="ID_1664226227" MODIFIED="1520420523211" TEXT="app-hat-member-enterprise-bankcard&#x7ed1;&#x5361;">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#23828;&#20271;&#23493;
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="button_ok"/>
+<node CREATED="1520051099718" ID="ID_223328381" MODIFIED="1520051102747" TEXT="&#x7ed1;&#x5361;"/>
+<node CREATED="1520051103095" ID="ID_1634625760" MODIFIED="1520051110732" TEXT="&#x89e3;&#x7ed1;"/>
+<node CREATED="1520051115059" ID="ID_1423680486" MODIFIED="1520051119952" TEXT="&#x67e5;&#x8be2;&#x7ed1;&#x5361;&#x5217;&#x8868;"/>
+</node>
+</node>
+<node CREATED="1520218814767" ID="ID_5225400" MODIFIED="1520234279947" TEXT="&#x8d26;&#x6237;">
+<node CREATED="1520234363409" ID="ID_1110974641" MODIFIED="1520420399035" TEXT="app-hat-account-query&#x8bfb;">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#21346;&#28925;
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="button_ok"/>
+<node CREATED="1520050958764" ID="ID_1025184341" MODIFIED="1520050964528" TEXT="&#x67e5;&#x8be2;&#x4f59;&#x989d;&#x660e;&#x7ec6;"/>
+<node CREATED="1520050954527" ID="ID_325355917" MODIFIED="1520050958348" TEXT="&#x67e5;&#x8be2;&#x4f59;&#x989d;"/>
+</node>
+<node CREATED="1520234368058" ID="ID_1654920008" MODIFIED="1520420391248" TEXT="app-hat-account&#x5199;">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#24352;&#36745;
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="button_ok"/>
+<node CREATED="1520051056366" ID="ID_222373230" MODIFIED="1520051059591" TEXT="&#x51bb;&#x7ed3;"/>
+<node CREATED="1520051060175" ID="ID_1146344686" MODIFIED="1520051067858" TEXT="&#x89e3;&#x51bb;"/>
+</node>
+</node>
+<node CREATED="1520229967530" ID="ID_230092318" MODIFIED="1520229976412" TEXT="&#x6620;&#x5c04;&#x5173;&#x7cfb;">
+<node CREATED="1520234258995" ID="ID_1510947937" MODIFIED="1520420477216" TEXT="app-hat-member-idmap-query&#x8bfb;">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#24352;&#36745;
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="button_ok"/>
+<node CREATED="1520234087695" ID="ID_18105953" MODIFIED="1520234090866" TEXT="&#x67e5;&#x8be2;"/>
+</node>
+<node CREATED="1520234263225" ID="ID_1469009585" MODIFIED="1520420489827" TEXT="app-hat-member-idmap&#x5199;">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#24352;&#36745;
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="button_ok"/>
+<node CREATED="1520234078053" ID="ID_582135481" MODIFIED="1520234081206" TEXT="&#x6ce8;&#x518c;"/>
+<node CREATED="1520234083178" ID="ID_1914548675" MODIFIED="1520234087208" TEXT="&#x6ce8;&#x9500;"/>
+</node>
+</node>
+</node>
+<node CREATED="1520230763301" ID="ID_501469114" MODIFIED="1520230765164" POSITION="right" TEXT="&#x6d3b;&#x52a8;">
+<node CREATED="1520218880540" ID="ID_1346907750" MODIFIED="1520218885128" TEXT="&#x4ea4;&#x6613;">
+<node CREATED="1520230963517" ID="ID_257486068" MODIFIED="1520420570657" TEXT="app-hat-trade&#x4ea4;&#x6613;&#x670d;&#x52a1;">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#21608;&#20043;&#39034;&#12289;&#21016;&#27915;&#12289;&#35885;&#25991;&#26519;&#12289;&#24464;&#26126;&#26126;&#12289;&#23433;&#20195;&#25104;
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="button_ok"/>
+<node CREATED="1520321292690" ID="ID_963038821" MODIFIED="1520321305472" TEXT="&#x4e0b;&#x5355;"/>
+<node CREATED="1520321308506" ID="ID_1557826621" MODIFIED="1520321311854" TEXT="&#x652f;&#x4ed8;"/>
+<node CREATED="1520321312111" ID="ID_874937441" MODIFIED="1520321316743" TEXT="&#x9000;&#x8d27;"/>
+<node CREATED="1520321317021" ID="ID_94167368" MODIFIED="1520321324111" TEXT="&#x63d0;&#x73b0;"/>
+<node CREATED="1520321324799" ID="ID_677183268" MODIFIED="1520321335658" TEXT="&#x5145;&#x503c;"/>
+</node>
+<node CREATED="1520230966281" ID="ID_1726507410" MODIFIED="1520420584550" TEXT="app-hat-trade-notify&#x901a;&#x77e5;">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#21608;&#20043;&#39034;&#12289;&#21016;&#27915;&#12289;&#35885;&#25991;&#26519;&#12289;&#24464;&#26126;&#26126;&#12289;&#23433;&#20195;&#25104;
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="button_ok"/>
+<node CREATED="1520236289498" ID="ID_422185770" MODIFIED="1520236718789" TEXT="&#x63a5;&#x6536;&#x5916;&#x90e8;&#x652f;&#x4ed8;&#x7ed3;&#x679c;&#x901a;&#x77e5;"/>
+<node CREATED="1520236276585" ID="ID_11555363" MODIFIED="1520236729267" TEXT="&#x901a;&#x77e5;&#x5916;&#x90e8;&#x5546;&#x6237;&#x8ba2;&#x5355;&#x7ed3;&#x679c;"/>
+</node>
+<node CREATED="1520324002122" ID="ID_874874255" MODIFIED="1520420600445" TEXT="app-hat-trade-&#x8865;&#x507f;">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#21608;&#20043;&#39034;&#12289;&#21016;&#27915;&#12289;&#35885;&#25991;&#26519;&#12289;&#24464;&#26126;&#26126;&#12289;&#23433;&#20195;&#25104;
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1520324005877" ID="ID_1960429957" MODIFIED="1520420643029" TEXT="app-hat-trade-query&#x67e5;&#x8be2;">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#21608;&#20043;&#39034;&#12289;&#21016;&#27915;&#12289;&#35885;&#25991;&#26519;&#12289;&#24464;&#26126;&#26126;&#12289;&#23433;&#20195;&#25104;
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="button_ok"/>
+<node CREATED="1520236372564" ID="ID_1366979079" MODIFIED="1520236377206" TEXT="&#x67e5;&#x8be2;&#x8be6;&#x5355;"/>
+<node CREATED="1520236377560" ID="ID_1060763614" MODIFIED="1520236380148" TEXT="&#x67e5;&#x8be2;&#x6d41;&#x6c34;"/>
+</node>
+</node>
+<node CREATED="1520230830846" ID="ID_1857963052" MODIFIED="1520230832363" TEXT="&#x7ed3;&#x7b97;">
+<node CREATED="1520230980269" ID="ID_1883152673" MODIFIED="1520421077808" TEXT="app-hat-settle&#x7ed3;&#x7b97;&#x670d;&#x52a1;">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#27431;&#38451;&#12289;&#21016;&#25935;
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1520230983372" ID="ID_1552433260" MODIFIED="1520421087138" TEXT="app-hat-settle-notify&#x901a;&#x77e5;">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#27431;&#38451;&#12289;&#21016;&#25935;
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="button_ok"/>
+<node CREATED="1520236352131" ID="ID_198771273" MODIFIED="1520236365424" TEXT="&#x63a5;&#x6536;MCS&#x7ed3;&#x7b97;&#x901a;&#x77e5;"/>
+</node>
+</node>
+<node CREATED="1520230832672" ID="ID_919311434" MODIFIED="1520230834913" TEXT="&#x5bf9;&#x8d26;">
+<node CREATED="1520236472150" ID="ID_1410451888" MODIFIED="1520236477994" TEXT="DTS"/>
+</node>
+</node>
+</node>
+</map>
