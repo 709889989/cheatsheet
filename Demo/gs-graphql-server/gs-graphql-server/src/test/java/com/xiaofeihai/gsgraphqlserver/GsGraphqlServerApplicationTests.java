7f3d5edc9594d999a9df9040571f595cdc4abde2
xumingming <mingming.xu@howbuy.com>
2024-09-14 17:37:27 +0800

    add: Spring for GraphQL

---
+++ Demo/gs-graphql-server/gs-graphql-server/src/test/java/com/xiaofeihai/gsgraphqlserver/GsGraphqlServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaofeihai.gsgraphqlserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsGraphqlServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
